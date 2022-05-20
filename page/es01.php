c3d10b8bcad65f7cd03fad54294d046562391db8
Emanuele Moncada <em@zerowk.it>
2022-05-20 10:55:29 +0200

    commit

--- page/es01.php
+++ page/es01.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ES || HELLO WORLD</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <hr>
+<h4>ES : Scrivi un programma in php che stampi "Hello WORLD"</h4>
+<hr>
+</body>
+</html>
+
+
 <?php
-echo "MELO"
+echo "Ciao, MELO"
 ?>
