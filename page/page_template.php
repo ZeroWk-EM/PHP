9a409e7bf800c7b7444f2b85cc52cb62359f11d2
Emanuele Moncada <em@zerowk.it>
2022-05-20 15:16:00 +0200

    modify css & html

---
+++ page/page_template.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ES || </title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <hr>
+    <p class="to_index"><a href="../">Torna all'index</a></p>
+    <hr>
+    <div class="block">
+        <div class="es">
+            <h4>ES : </h4>
+        </div>
+    </div>
+    <div class="elaboration">
+        <p style="text-align:center">OUTPUT</p>
+    </div>
+    <div class="container">
+        <div class="code">
+            <?php
+            //CODICE PHP
+            ?>
+        </div>
+    </div>
+    <hr>
+    <p style="text-align:right;">FINITO XX/XX/20XX || XX:XX</p>
+</body>
+
+</html>
