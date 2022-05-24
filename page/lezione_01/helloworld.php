fb328ff6978ce1d529352c837770055c7dd39fa9
Emanuele Moncada <em@zerowk.it>
2022-05-24 10:09:19 +0200

    update file

--- page/lezione_01/helloworld.php
+++ page/lezione_01/helloworld.php
@@ -14,20 +14,27 @@
     <hr>
     <p class="to_index"><a href="../../index.html">Torna all'index</a></p>
     <hr>
+
     <div class="block">
         <div class="es">
             <h4>ES : Scrivi un programma in php che stampi "Hello, World!"</h4>
         </div>
     </div>
+
     <div class="elaboration">
         <p style="text-align:center">OUTPUT</p>
     </div>
+
     <div class="container">
         <div class="code">
             <?php
             echo "Hello, World!";
             ?>
         </div>
+    </div>
+
+    <div class="phpcode">
+        <pre>echo "Hello, World!";</pre>
     </div>
     <hr>
     <p style="text-align:right;">FINITO 20/05/2022 || 13:00</p>
