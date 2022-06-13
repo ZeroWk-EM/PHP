f864f5d605dfe3c429f526c10251df7255a48381
Emanuele Moncada <em@zerowk.it>
2022-06-13 15:15:57 +0200

    upload exercises

--- page/lezione_02/multidimensional_array.php
+++ page/lezione_02/multidimensional_array.php
@@ -6,19 +6,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ES || ARRAY MULTIDIMENSIONALI</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
     <link rel="icon" type="image/x-icon" href="asset/php.ico">
 
 </head>
 
 <body>
     <hr>
-    <p class="to_index"><a href="../">Torna all'index</a></p>
+    <p class="to_index"><a href="../../index.html">Torna all'index</a></p>
     <hr>
     <div class="block">
         <div class="es">
-            <h4>ES : </h4>
+            <h4>ES : Creare un programma che permetta la gestione di un array multidimensionale</h4>
+            <p>La chiave dell'array è rappresentato da un codice fiscale.
+                Ogni elemento è un array associativo con le seguenti informazioni:
+            <ul>
+                <li>Nome</li>
+                <li>Cognome</li>
+                <li>Professione</li>
+                <li>Citta</li>
+                <li>Eta</li>
+            </ul>
+
+            Per generare e popolare l'array creare le seguenti funzioni:
+
+            <ul>
+                <li>popolareArray(N) <p>Dove N è il numero di elementi dell'array</p>
+                </li>
+                <li>generaNome(...)</li>
+                <li>generaCognome(...)</li>
+                <li>generaProfessione(...) </li>
+                <li>generaCitta(...) </li>
+                <li>generaEta(...) </li>
+                <li>generaCF(...)</li>
+            </ul>
+
+            Un volta popolato l'array implementare e seguenti funzioni:
+            <ul>
+                <li>stampa(CF)</li>
+                <li>stampaOrdineAlfabetico($ordine,...) <p>Ordine per definire se ordinare in modo decrescente o ascendente</p>
+                </li>
+                <li>stampaOrdineETA($ordine,...) <p>Ordine per definire se ordinare in modo decrescente o ascendente</p>
+                </li>
+                <li>ricercaPerNome(...)</li>
+                <li>ricercaPerProfessione(...)</li>
+                <li> eliminaPersona(...</li>
+            </ul>
+            </p>
         </div>
     </div>
     <div class="elaboration">
@@ -27,7 +62,7 @@
     <div class="container">
         <div class="code">
             <?php
-            //CODICE PHP
+
             ?>
         </div>
     </div>
