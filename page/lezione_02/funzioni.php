b1440db86bda44e14b497ae2373770603bbc05d6
Emanuele Moncada <em@zerowk.it>
2022-06-17 13:24:46 +0200

    update reference to css

--- page/lezione_02/funzioni.php
+++ page/lezione_02/funzioni.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ES || FUNZIONI ARRAY</title>
     <link rel="stylesheet" href="../css/style.css">
-    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
-    <link rel="icon" type="image/x-icon" href="asset/php.ico">
+    <link rel="icon" type="image/x-icon" href="../../asset/php.ico">
+
 
 </head>
 
---
+++ page/lezione_03/method.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ES || METODI </title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" type="image/x-icon" href="../../asset/php.ico">
+</head>
+
+<body>
+    <hr>
+    <p class="to_index"><a href="../../index.html">Torna all'index</a></p>
+    <hr>
+    <div class="block">
+        <div class="es">
+            <h4>ES : </h4>
+        </div>
+    </div>
+    <div class="elaboration">
+        <p style="text-align:center">OUTPUT</p>
+    </div>
+    <div class="container">
+        <div class="code">
+            <?php
+            //CODICE PHP
+            ?>
+        </div>
+    </div>
+    <hr>
+    <p style="text-align:right;">FINITO XX/XX/20XX || XX:XX</p>
+</body>
+
+</html>
